592e8a471c939cb554b0ea24d0356bad9362657b
Serkan Kilicdere <serkan.kilicdere@outlook.com>
2025-09-17 20:19:08 +0300

    Question 013 added without answers.

---
+++ src/main/java/com/bilgeadam/Question_013.java
@@ -0,0 +1,21 @@
+package com.bilgeadam;
+// TODO SOLVE THE QOUESTION 013
+/**
+ *      * * * * * * * * *
+ *      * * * * * * * * *
+ *      * * * * * * * * *
+ *      * * * * + * * * *
+ *      * * * * * * * * *
+ *      * * * * * * * * *
+ *      * * * * * * * * *
+ *
+ *      in the above pattern with direction button move plus sign without limit
+ *
+ *
+ */
+
+public class Question_013 {
+    public static void main(String[] args) {
+
+    }
+}
